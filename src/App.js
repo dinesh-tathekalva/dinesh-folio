cc47e3f601fd0b3d341e5380627ab584fff90923
Mickey <mickey@Mickeys-MacBook-Pro.local>
2020-03-28 00:45:09 -0500

    WIP Testing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-
-
-
-
+import React, { Component } from 'react';
 import './App.css';
+import Container from './components/Container'
+import Avatar from './components/Avatar'
+import Tooltip from './components/ToolTip'
+import Paper from './components/Paper'
+// import Skills from './Skill.json'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
-      <p>Hello</p>
+      <body>
+        <Container> 
+          <Paper elevation={0} />
+        </Container>
+      </body>
     </div>
   );
 }
-
 export default App;
